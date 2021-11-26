03279a5f5ae77d07ed13797f3dd92ea248b35fd7
yangyongqi18 <yangyongqi18@jd.com>
2021-11-26 09:38:31 +0800

    解决中文乱码问题

--- HttpUtil.cpp
+++ HttpUtil.cpp
@@ -8,7 +8,7 @@ void httpPost(WCHAR buffer[]) {
     {
         http::Request request{ "http://localhost:8080/test" };
         // send a post request
-        const std::string body = CovenWCHARToString(buffer);
+        const std::string body = WCHAR2String(buffer);
         const auto response = request.send("POST", body, {
             "Content-Type: application/json; charset=utf-8"
             });
@@ -25,4 +25,24 @@ std::string CovenWCHARToString(WCHAR buffer[]) {
     wchar_t* WC = buffer;
     wcstombs(MB, WC, BUFFER_SIZE);
     return MB;
+}
+
+std::string WCHAR2String(LPCWSTR pwszSrc){
+    int nLen = WideCharToMultiByte(CP_ACP,0, pwszSrc, -1, NULL, 0, NULL, NULL);
+    if (nLen <= 0)
+    {
+        return std::string("");
+    }
+
+    char* pszDst = new  char[nLen];
+    if (NULL == pszDst)
+    {
+        return std::string("");
+    }
+
+    WideCharToMultiByte(CP_ACP,0, pwszSrc, -1, pszDst, nLen, NULL,NULL);
+    pszDst[nLen - 1] = 0;
+    std::string strTemp(pszDst);
+    delete [] pszDst;
+    return strTemp;
 }
